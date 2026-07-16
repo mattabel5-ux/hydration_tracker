ef96e95c8d9b1340fdf663bd3aaf28e6fc4c44a7
Pumafootball5 <matthew.abel@talktotucker.com>
2026-07-16 10:13:30 -0400

    updated AppDelegate.swift file to to show the banners even when the app is open

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,13 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // --- NEW: Forces iOS to show notifications in the foreground ---
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    // ---------------------------------------------------------------
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
